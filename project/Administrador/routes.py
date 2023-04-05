1266ff1cec3d4dda4307f47d94c408a663f6b1c6
IDGS-802-20002145 <20002145@alumnos.utleon.edu.mx>
2023-04-04 18:30:06 -0600

    Acomodo de la estructura y creacion para el modulo de proveedores

---
+++ project/Administrador/routes.py
@@ -0,0 +1,131 @@
+import os
+import uuid
+from flask import Blueprint, render_template, flash, redirect, request, url_for, current_app
+from flask_security import login_required, current_user
+from flask_security.decorators import roles_required, roles_accepted
+from ..models import db
+from project.models import Products, Role
+from werkzeug.utils import secure_filename
+import logging
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+
+administrador = Blueprint('administrador', __name__)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler('flask.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+
+# Definimos la ruta para la página de perfil de usuairo
+
+
+@administrador.route('/administrador')
+@login_required
+@roles_required('admin')
+def admin():
+    productos = Products.query.all()
+    return render_template('RopaCrud.html', productos=productos)
+
+
+@administrador.route('/administrador', methods=['POST'])
+@login_required
+@roles_required('admin')
+def admin_post():
+    img=str(uuid.uuid4())+'.png'
+    imagen=request.files['image']
+    ruta_imagen = os.path.abspath('project\\static\\img')
+    imagen.save(os.path.join(ruta_imagen,img))       
+    alum=Products(nombre=request.form.get('txtNombre'),
+                    descripcion=request.form.get('txtDescripcion'),
+                    estilo=request.form.get('txtEstilo'),
+                    precio=request.form.get('txtPrecio'),
+                    image=img)
+        #Con esta instruccion guardamos los datos en la bd
+    db.session.add(alum)
+    db.session.commit()
+    flash("El registro se ha guardado exitosamente.", "exito")
+    return redirect(url_for('main.principalAd'))
+    
+
+@administrador.route('/modificar', methods=['GET', 'POST'])
+@login_required
+def modificar():
+    if request.method == 'GET':
+        id = request.args.get('id')
+        producto = Products.query.get(id)
+        print(producto)
+        if producto is None:
+            flash("El pzroducto no existe", "error")
+            return redirect(url_for('administrador.admin'))
+        if not producto.image:
+            producto.image = 'default.png' # o cualquier otro valor predeterminado para la imagen
+        return render_template('modificar.html', producto=producto,id=id)
+    elif request.method == 'POST':
+        id = request.args.get('id')
+        producto = Products.query.get(id)
+        print(producto)
+        if producto is None:
+            flash("El producto no existe", "error")
+            return redirect(url_for('administrador.admin'))
+        producto.nombre = request.form.get('txtNombre')
+        producto.estilo = request.form.get('txtEstilo')
+        producto.descripcion = request.form.get('txtDescripcion')
+        producto.precio = request.form.get('txtPrecio')
+        imagen = request.files.get('image')
+        ruta_imagen = os.path.abspath('project\\static\\img')
+        if imagen:
+            # Eliminar la imagen anterior
+            os.remove(os.path.join(ruta_imagen, producto.image))
+            # Guardar la nueva imagen
+            filename = secure_filename(imagen.filename)
+            imagen.save(os.path.join(ruta_imagen, filename))
+            producto.image = filename
+        db.session.commit()
+        flash("El registro se ha modificado exitosamente.", "exito")
+        return redirect(url_for('main.principalAd'))
+
+@administrador.route('/eliminar', methods=['GET', 'POST'])
+@login_required
+def eliminar():
+    if request.method == 'GET':
+        id = request.args.get('id')
+        producto = Products.query.get(id)
+        print(producto)
+        if producto is None:
+            flash("El producto no existe", "error")
+            return redirect(url_for('administrador.admin'))
+        if not producto.image:
+            producto.image = 'default.png' # o cualquier otro valor predeterminado para la imagen
+        return render_template('eliminar.html', producto=producto,id=id)
+    elif request.method == 'POST':
+        id = request.args.get('id')
+        producto = Products.query.get(id)
+        print(producto)
+        if producto is None:
+            flash("El producto no existe", "error")
+            return redirect(url_for('administrador.admin'))
+        producto.nombre = request.form.get('txtNombre')
+        producto.estilo = request.form.get('txtEstilo')
+        producto.descripcion = request.form.get('txtDescripcion')
+        producto.precio = request.form.get('txtPrecio')
+        imagen = request.files.get('image')
+        ruta_imagen = os.path.abspath('project\\static\\img')
+        if imagen:
+            # Eliminar la imagen anterior
+            os.remove(os.path.join(ruta_imagen, producto.image))
+            # Guardar la nueva imagen
+            filename = secure_filename(imagen.filename)
+            imagen.save(os.path.join(ruta_imagen, filename))
+            producto.image = filename
+        db.session.delete(producto)
+        db.session.commit()
+        flash("El registro se ha eliminado exitosamente.", "exito")
+        return redirect(url_for('main.principalAd'))
+    
+@administrador.route('/proveedores', methods=['GET', 'POST'])
+@login_required
+def proveedores():
+    if request.method == 'GET':
+        return render_template('proveedores.html')
--- project/__init__.py
+++ project/__init__.py
@@ -36,6 +36,11 @@ def create_app():
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
+
+    from .Administrador.routes import administrador as admin_blueprint
+    app.register_blueprint(admin_blueprint)
+
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
