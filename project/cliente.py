32a0ee981b30e6635c064148659c21639b0881cf
IDGS-802-20001492 <quirozbernardo2@gmail.com>
2023-04-07 16:30:59 -0600

    Avance de pedidos y catalogo

--- project/cliente.py
+++ project/cliente.py
@@ -4,11 +4,11 @@ from flask import Blueprint, render_template, flash, redirect, request, url_for,
 from flask_security import login_required, current_user
 from flask_security.decorators import roles_required, roles_accepted
 from . import db
-from project.models import Products, Role
+from project.models import DetPedido, Pedido, Producto, Products, Role
 from werkzeug.utils import secure_filename
 import logging
 from logging.handlers import RotatingFileHandler
-from datetime import datetime
+from datetime import date, datetime
 
 cliente = Blueprint('cliente', __name__)
 logger = logging.getLogger(__name__)
@@ -28,3 +28,60 @@ def principalAd():
     print(current_user.admin)
 
     return render_template('principalAd.html', productos=productos)
+
+@cliente.route('/pedidos',methods=["GET","POST"])
+@login_required
+def pedidos():
+    ids_pedidos = Pedido.query.with_entities(Pedido.id).filter_by(user_id= current_user.id).all()
+    detPed = DetPedido.query.filter(DetPedido.pedido_id.in_([id_pedido[0] for id_pedido in ids_pedidos])).all()
+    
+    producto_ids = [dp.producto_id for dp in detPed]
+
+    # Hacer la consulta para obtener solo los productos con las IDs correspondientes
+    productos = Producto.query.filter(Producto.id.in_(producto_ids)).all()
+    
+    if request.method == 'POST':
+        #Datos del pedido
+        userID = current_user.id
+        cantidad = 1
+        fecha_actual = date.today()
+        fecha_actual_str = fecha_actual.strftime('%Y-%m-%d')
+        #obten la fecha actual sin la hora
+        # 1 Es pedido , 2 es compra y 0 es eliminado
+        estatus = 1
+        print(userID)
+        pedido = Pedido(user_id = userID,
+                        cantidad = cantidad,
+                        fecha = fecha_actual_str,
+                        estatus = estatus)
+        db.session.add(pedido)
+        db.session.commit()
+        
+        #Datos del detalle del pedido
+        consPedido = Pedido.query.filter_by(id=pedido.id).first()
+        if consPedido:
+            id_obtenido = consPedido.id
+        pedidoID = id_obtenido
+        productoID = request.args.get('idProducto')
+        cantidad = pedido.cantidad
+                 
+        detPed = DetPedido(pedido_id = pedidoID, producto_id = productoID,cantidad = cantidad)
+        db.session.add(detPed)
+        db.session.commit()
+        return redirect(url_for('cliente.catalogoC'))
+    
+    return render_template('./pedidos/pedidos.html', detPed = detPed, productos = productos)
+
+@cliente.route('/eliminarPedido',methods=["GET","POST"])
+@login_required
+def eliminarPedido():
+    if request.method == 'POST':
+        return 0
+    return 0
+
+@cliente.route('/catalogoC',methods=["GET","POST"])
+@login_required
+def catalogoC():
+    prod = Producto.query.all()
+    return render_template('catalogoCliente.html', productos = prod)
+
--- project/models.py
+++ project/models.py
@@ -42,5 +42,96 @@ class Products(db.Model):
     descripcion= db.Column(db.String(255))
     image = db.Column(db.String(255),nullable=False)
     active= db.Column(db.Boolean,default=1)
-        
+
+class Producto(db.Model):
+    __tablename__ = 'producto'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    descripcion = db.Column(db.Text)
+    talla = db.Column(db.String(50), nullable=False)
+    color = db.Column(db.String(50), nullable=False)
+    modelo = db.Column(db.String(50), nullable=False)
+    precio = db.Column(db.Float, nullable=False)
+    imagen = db.Column(db.String(250), nullable=False)
+    stock_existencia = db.Column(db.Float, nullable=False)
+    estatus = db.Column(db.Boolean, default=True)
+    det_pedido = db.relationship('DetPedido', backref='producto_detalle', lazy=True, overlaps="producto_detalle")
+    explotacion_material = db.relationship('ExplotacionMaterial', backref='producto_explosion', lazy=True, overlaps="producto_explosion")
+    det_venta = db.relationship('DetVenta', backref='producto', lazy=True)
     
+class Pedido(db.Model):
+    __tablename__ = 'Pedido'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
+    fecha = db.Column(db.Date, nullable=False)
+    estatus = db.Column(db.Boolean, default=True)
+    detalles = db.relationship('DetPedido', backref='pedido_details', lazy=True, overlaps='pedido')
+    
+class DetPedido(db.Model):
+    __tablename__ = 'detpedido'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pedido_id = db.Column(db.Integer, db.ForeignKey('Pedido.id'), nullable=False)
+    producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
+    pedido = db.relationship('Pedido', overlaps="pedido_details")
+    producto = db.relationship('Producto', overlaps="det_pedido,producto_detalle")
+   
+class Venta(db.Model):
+    __tablename__ = 'venta'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    fecha = db.Column(db.Date, nullable=False)
+    det_venta = db.relationship('DetVenta', backref='venta', lazy=True)
+
+class DetVenta(db.Model):
+    __tablename__ = 'detventa'
+    id = db.Column(db.Integer, primary_key=True)
+    venta_id = db.Column(db.Integer, db.ForeignKey('venta.id'), nullable=False)
+    producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
+    precio = db.Column(db.Float, nullable=False)
+    
+class Proveedor(db.Model):
+    __tablename__ = 'Proveedor'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    telefono = db.Column(db.String(20))
+    direccion = db.Column(db.String(255))
+    estatus = db.Column(db.Boolean, default=True)
+    Compra = db.relationship('Compra', backref='Proveedor', lazy=True, overlaps='proveedor')         
+    
+class ExplotacionMaterial(db.Model):
+    __tablename__ = 'explotacion_material'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'), nullable=False)
+    material_id = db.Column(db.Integer, db.ForeignKey('inventario_materia_prima.id'), nullable=False)
+    cantidad_usada = db.Column(db.Float, nullable=False)
+    cantidadIndividual = db.Column(db.Float, nullable=False)
+    producto = db.relationship('Producto', backref='materiales_usados', overlaps="explotacion_material,producto_explosion")
+    material = db.relationship('InventarioMateriaPrima', overlaps='materiales_usados')
+
+class InventarioMateriaPrima(db.Model):
+    __tablename__ = 'inventario_materia_prima'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    descripcion = db.Column(db.Text)
+    cantidad = db.Column(db.Integer, nullable=False)
+    stock_minimo = db.Column(db.Float)
+
+class Compra(db.Model):
+    __tablename__ = 'Compra'
+    id = db.Column(db.Integer, primary_key=True)
+    proveedor_id = db.Column(db.Integer, db.ForeignKey('Proveedor.id'), nullable=False)
+    fecha = db.Column(db.Date, nullable=False)
+    DetCompra = db.relationship('DetCompra', backref='Compra', lazy=True, overlaps="Compra")
+    
+class DetCompra(db.Model):
+    __tablename__ = 'DetCompra'
+    id = db.Column(db.Integer, primary_key=True)
+    compra_id = db.Column(db.Integer, db.ForeignKey('Compra.id'), nullable=False)
+    material_id = db.Column(db.Integer, db.ForeignKey('inventario_materia_prima.id'), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
+    precio = db.Column(db.Float, nullable=False)
+    compra = db.relationship('Compra', overlaps="Compra")
